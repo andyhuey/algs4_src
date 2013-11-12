a743a302225d5e1ec50c9d63829e01753f789185
Andrew Huey <andyhuey@users.noreply.github.com>
2013-11-12 13:48:34 -0500

    add (mostly) empty files for assn #3

---
+++ algs1-assn3/Point.java
@@ -0,0 +1,68 @@
+/*************************************************************************
+ *  Author:        Andrew Huey
+ *  Written:       11/12/2013
+ *  Last updated:  11/12/2013
+ *
+ * Compilation:  javac Point.java
+ * Execution:
+ * Dependencies: StdDraw.java
+ *
+ * Description: An immutable data type for points in the plane.
+ * 
+ *  https://class.coursera.org/algs4partI-003/assignment/view?assignment_id=4
+ *  http://coursera.cs.princeton.edu/algs4/assignments/collinear.html
+ *  http://coursera.cs.princeton.edu/algs4/checklists/collinear.html
+ *
+ *************************************************************************/
+
+import java.util.Comparator;
+
+public class Point implements Comparable<Point> {
+
+    // compare points by slope
+    public final Comparator<Point> SLOPE_ORDER;       // YOUR DEFINITION HERE
+
+    private final int x;                              // x coordinate
+    private final int y;                              // y coordinate
+
+    // create the point (x, y)
+    public Point(int x, int y) {
+        /* DO NOT MODIFY */
+        this.x = x;
+        this.y = y;
+    }
+
+    // plot this point to standard drawing
+    public void draw() {
+        /* DO NOT MODIFY */
+        StdDraw.point(x, y);
+    }
+
+    // draw line between this point and that point to standard drawing
+    public void drawTo(Point that) {
+        /* DO NOT MODIFY */
+        StdDraw.line(this.x, this.y, that.x, that.y);
+    }
+
+    // slope between this point and that point
+    public double slopeTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    // is this point lexicographically smaller than that one?
+    // comparing y-coordinates and breaking ties by x-coordinates
+    public int compareTo(Point that) {
+        /* YOUR CODE HERE */
+    }
+
+    // return string representation of this point
+    public String toString() {
+        /* DO NOT MODIFY */
+        return "(" + x + ", " + y + ")";
+    }
+
+    // unit test
+    public static void main(String[] args) {
+        /* YOUR CODE HERE */
+    }
+}
