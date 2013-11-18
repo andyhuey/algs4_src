a07da52dc00fd0659dddecfff4487ca5937cc81d
Andrew Huey <andyhuey@users.noreply.github.com>
2013-11-18 16:39:16 -0500

    a little work on Board.java

--- algs1-assn4/Board.java
+++ algs1-assn4/Board.java
@@ -13,14 +13,76 @@
 
 
 public class Board {
-    public Board(int[][] blocks)           // construct a board from an N-by-N array of blocks
-                                           // (where blocks[i][j] = block in row i, column j)
-    public int dimension()                 // board dimension N
-    public int hamming()                   // number of blocks out of place
-    public int manhattan()                 // sum of Manhattan distances between blocks and goal
-    public boolean isGoal()                // is this board the goal board?
-    public Board twin()                    // a board obtained by exchanging two adjacent blocks in the same row
-    public boolean equals(Object y)        // does this board equal y?
-    public Iterable<Board> neighbors()     // all neighboring boards
-    public String toString()               // string representation of the board (in the output format specified below)
+    // probably:
+    private int N;
+    private int[][] tiles;
+    // ...and a MinPQ priority queue?
+    
+    // construct a board from an N-by-N array of blocks
+    // (where blocks[i][j] = block in row i, column j)
+    public Board(int[][] blocks)
+    {
+        //todo
+    }
+    
+    // board dimension N
+    public int dimension()
+    {
+        //todo
+    }
+    
+    // number of blocks out of place
+    public int hamming()
+    {
+        //todo
+    }
+    
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan()
+    {
+        //todo
+    }
+    
+    // is this board the goal board?
+    public boolean isGoal()
+    {
+        //todo
+    }
+    
+    // a board obtained by exchanging two adjacent blocks in the same row
+    public Board twin()
+    {
+        //todo
+    }
+    
+    // does this board equal y?
+    public boolean equals(Object y)
+    {
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        //todo
+        
+    }
+    
+    // all neighboring boards
+    public Iterable<Board> neighbors()
+    {
+        //todo
+    }
+    
+    // string representation of the board (in the output format specified below)
+    public String toString()
+    {
+        //todo
+        StringBuilder s = new StringBuilder();
+        s.append(N + "\n");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();        
+    }
 }
